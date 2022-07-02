54488a047ccc4721c3796f91247f312ce2fa4abd
Tiramister <nh111424@gmail.com>
2022-07-02 16:30:37 +0900

    feat(App.vue): 主コンテナを MainContainer.vue に分離

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,22 +2,20 @@
   <v-app>
     <CommonHeader />
     <v-main>
-      <v-container>
-        <HelloWorld />
-      </v-container>
+      <MainContainer />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 import CommonHeader from "./components/CommonHeader.vue";
+import MainContainer from "./components/MainContainer.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    MainContainer,
     CommonHeader,
   },
 });
---
+++ frontend/src/components/MainContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <h1>UNIMPLEMENTED</h1>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainContainer",
+});
+</script>
