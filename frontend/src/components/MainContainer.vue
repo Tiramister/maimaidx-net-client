aab516876e901a63b1ae9b1fae07194e6799ecc4
Tiramister <nh111424@gmail.com>
2022-07-10 16:30:04 +0900

    feat(MainContainer): 表の中身を作成

--- frontend/src/components/MainContainer.vue
+++ frontend/src/components/MainContainer.vue
@@ -12,7 +12,17 @@
           <th>バッジ</th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        <tr v-for="record in records" :key="getId(record)">
+          <td>{{ record.title }}</td>
+          <td>{{ record.type }}</td>
+          <td>{{ record.difficulty }}</td>
+          <td>{{ record.level }}</td>
+          <td>{{ record.score / 10000 }} %</td>
+          <td></td>
+          <td>{{ record.badge }}</td>
+        </tr>
+      </tbody>
     </v-table>
   </v-container>
 </template>
@@ -22,5 +32,32 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "MainContainer",
+  data() {
+    return {
+      records: [
+        {
+          title: "星空パーティーチューン",
+          type: "DX",
+          difficulty: "MAS",
+          level: "13+",
+          score: 1006837,
+          badge: "FC+",
+        },
+        {
+          title: "大輪の魂 (feat. AO, 司芭扶)",
+          type: "DX",
+          difficulty: "MAS",
+          level: "13",
+          score: 988293,
+          badge: "",
+        },
+      ],
+    };
+  },
+  methods: {
+    getId(record: any) {
+      return record.title + "-" + record.type + "-" + record.difficulty;
+    },
+  },
 });
 </script>
