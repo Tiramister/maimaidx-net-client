40abc686506471838c103c16562cb673a01ab959
Tiramister <nh111424@gmail.com>
2022-07-16 05:37:35 +0900

    feat(main.rs): reqwest で https://maimaidx.jp/maimai-mobile/ にアクセス

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let client = reqwest::Client::new();
+    let login_page = client
+        .get("https://maimaidx.jp/maimai-mobile/")
+        .send()
+        .await?
+        .text()
+        .await?;
+    println!("{login_page}");
+    Ok(())
 }
