ffa345be7d254563a5d54a95029b80e4e9147454
Tiramister <nh111424@gmail.com>
2022-07-20 01:48:20 +0000

    refactor(get_login_token): reqwest::Client を引数に取るように変更

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,16 @@
 use anyhow::Result;
+use reqwest::Client;
 use scraper::{Html, Selector};
 
-async fn get_login_token() -> Result<String> {
-    let login_page = reqwest::get("https://maimaidx.jp/maimai-mobile/")
+async fn get_login_token(client: &Client) -> Result<String> {
+    let login_page = client
+        .get("https://maimaidx.jp/maimai-mobile/")
+        .send()
         .await?
         .text()
         .await?;
     let document = Html::parse_document(&login_page);
+
     let token_selector = Selector::parse("input[name='token']").unwrap();
     let token_element = document.select(&token_selector).next().unwrap();
     let token = token_element.value().attr("value").unwrap();
@@ -15,7 +19,8 @@ async fn get_login_token() -> Result<String> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let token = get_login_token().await?;
+    let client = Client::new();
+    let token = get_login_token(&client).await?;
     println!("{token}");
     Ok(())
 }
