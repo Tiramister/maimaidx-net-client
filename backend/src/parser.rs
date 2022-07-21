9bd0a95773610d160335b44ca229128da87f2f16
Tiramister <nh111424@gmail.com>
2022-07-21 11:49:05 +0000

    refactor(parser.rs): select_some_element と select_first_element を実装

---
+++ backend/src/parser.rs
@@ -0,0 +1,21 @@
+use anyhow::{anyhow, Result};
+use scraper::{ElementRef, Selector};
+
+pub fn select_some_element<'a>(
+    element: &'a ElementRef,
+    selector_str: &str,
+) -> Result<Option<ElementRef<'a>>> {
+    // select
+    let selector = Selector::parse(selector_str)
+        .map_err(|_| anyhow!("failed to parse the selector {}", selector_str))?;
+    let mut select = element.select(&selector);
+    Ok(select.next())
+}
+
+pub fn select_first_element<'a>(
+    element: &'a ElementRef,
+    selector_str: &str,
+) -> Result<ElementRef<'a>> {
+    select_some_element(element, selector_str)?
+        .ok_or(anyhow!("there is no element selected by {}", selector_str))
+}
